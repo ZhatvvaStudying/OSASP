37342f5852fe05fbaf7b5cdad9ee29637298b28b
ZhatvvaStudying <broybeb@gmail.com>
2022-02-19 00:30:32 +0300

    individual_task was added

---
+++ lab_1/individual_task.sh
@@ -0,0 +1,15 @@
+#!/bin/bash 
+argsCount=$#
+inpStr=$1
+dirPath=$2
+if [ $argsCount -ne 2 ]
+then
+    echo -e "Only 2 args must be passed\n1 - string to search\n2 - directory path"
+else
+    if ! [ -d "$dirPath" ]
+    then
+        echo "$dirPath isn't a directory"
+    else
+        grep -rl "$inpStr" "$dirPath" | xargs -d '\n' ls -lSr | awk '{print $5,$9}'      
+    fi
+fi
