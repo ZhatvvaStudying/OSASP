babc90f9884d2f410c37a931c57db7958418ece2
ZhatvvaStudying <broybeb@gmail.com>
2022-04-15 19:16:09 +0300

    Changed some var names

--- lab_2/individual.c
+++ lab_2/individual.c
@@ -4,39 +4,38 @@
 #include <string.h>
 #include <sys/vfs.h>
 
-char* GetFullName(char *buffer, char *folder, char *Dir)
+void SetFullPath(char *fullName, char *folder, char *dir)
 {
-	strcpy(buffer, folder);
+	strcpy(fullName, folder);
 	
-	if (buffer[strlen(buffer) - 1] != '/')
+	if (fullName[strlen(fullName) - 1] != '/')
     {
-        strcat(buffer, "/");
+        strcat(fullName, "/");
     }
 	
-	strcat(buffer, Dir);
-	return buffer;
+	strcat(fullName, dir);
 }
 
 long long GetDirSize(char* dirName, long capacity)
 {
-	DIR *dirp;		
+	DIR *dir;		
 	struct dirent *de;
 	struct stat buf;
 	long long size = 0;
 	char fullPath[1024];
 		
-	if ((dirp = opendir(dirName)) == NULL)
+	if ((dir = opendir(dirName)) == NULL)
     {
         perror("Cannot open directory");
     }
 	else 
     {
-		for (de = readdir(dirp); de != NULL; de = readdir(dirp))
+		for (de = readdir(dir); de != NULL; de = readdir(dir))
         {
-			GetFullName(fullPath,dirName,de->d_name);
-			
-			if (strcmp(de->d_name, ".") != 0 && strcmp(de->d_name, "..") !=0 && de->d_type == DT_DIR)
+			if (strcmp(de->d_name, ".") != 0 && strcmp(de->d_name, "..") != 0)
             {
+                SetFullPath(fullPath, dirName, de->d_name);
+                
                 if (de->d_type == DT_DIR)
                 {
                     size += GetDirSize(fullPath, capacity);
@@ -49,14 +48,13 @@ long long GetDirSize(char* dirName, long capacity)
                     }
                     else
                     {
-                        perror("Cannot get stats");
+                        perror("Cannot get stats for ");
                     }
                 }
-
             }
 		}	
 		printf("'%s'\t%lld bytes\t Disk usage %f%%\n", dirName, size, (float)size / capacity * 100);
-		if (closedir(dirp) == -1)
+		if (closedir(dir) == -1)
         {
             perror("Cannot close directory");
         }
@@ -67,7 +65,9 @@ long long GetDirSize(char* dirName, long capacity)
 
 int main(int argc, char* argv[]) {
 
-	if (argc != 2) 
+	argv[1] = ".";
+    argc = 2;
+    if (argc != 2) 
     {
 		fprintf(stderr,"Error! Invalid operands count!\n\t1 - directory name");
 		return 0;
